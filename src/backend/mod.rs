44f6857ceeac20443834062787fc9e57616fec14
alindima <alindima@amazon.com>
2021-07-16 16:01:29 +0300

    Add backend module
    Signed-off-by: alindima <alindima@amazon.com>

---
+++ src/backend/mod.rs
@@ -0,0 +1,4 @@
+// Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
+//! This module defines the data structures used for the intermmediate representation (IR),
+//! as well as the logic for compiling the filter into BPF code, the final form of the filter.
