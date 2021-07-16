27e9698dbb8c556ef7b8199623be16f39db0357e
alindima <alindima@amazon.com>
2021-07-16 16:01:29 +0300

    Add backend module
    Signed-off-by: alindima <alindima@amazon.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,19 @@
+// Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
 #![deny(missing_docs)]
 #![cfg(target_endian = "little")]
-//! Dummy crate needs high-level documentation.
-/// Dummy public function needs documentation.
-pub fn it_works() {
-    assert_eq!(1 + 1, 2);
-}
+//! Provides easy-to-use Linux seccomp-bpf jailing.
+//!
+//! Seccomp is a Linux kernel security feature which enables a tight control over what kernel-level
+//! mechanisms a process has access to. This is typically used to reduce the attack surface and
+//! exposed resources when running untrusted code. This works by allowing users to write and set a
+//! BPF (Berkeley Packet Filter) program for each process or thread, that intercepts syscalls and
+//! decides whether the syscall is safe to execute.
+//!
+//! Writing BPF programs by hand is difficult and error-prone. This crate provides high-level
+//! wrappers for working with system call filtering.
+
+mod backend;
+
+// Re-export the IR public types.
+pub use backend::*;
